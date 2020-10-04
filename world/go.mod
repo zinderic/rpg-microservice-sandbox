584271c6199275cb3768b706d5392d995eac410d
Kalin Staykov <k.t.staykov@gmail.com>
2020-10-04 14:12:56 +0300

    add weather to the world

--- world/go.mod
+++ world/go.mod
@@ -3,7 +3,7 @@ module world
 go 1.15
 
 require (
-	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20201002170205-7f63de1d35b0 // indirect
 )
--- world/main.go
+++ world/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/labstack/echo"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -13,5 +16,17 @@ func main() {
 	e.GET("/places", func(c echo.Context) error {
 		return c.String(http.StatusOK, "TODO - add some interesting places\n")
 	})
+	e.GET("/weather", Weather)
 	e.Logger.Fatal(e.Start(":3333"))
+}
+
+func Weather(c echo.Context) error {
+	weatherOptions := []string{
+		"snowing heavily",
+		"raining lightly",
+		"sunny",
+	}
+	rand.Seed(time.Now().Unix()) // initialize global pseudo random generator
+	weatherReport := fmt.Sprint(weatherOptions[rand.Intn(len(weatherOptions))], "\n")
+	return c.String(http.StatusOK, weatherReport)
 }

d4c541449a78adafb2cb4b1ac944c80d6eaace60
Kalin Staykov <k.t.staykov@gmail.com>
2020-10-30 18:02:47 +0200

    add some database code and bring back dev image config
    I also patch the app container to wait for its DB. I will
    consider adding that to prod profiles too at some point.

--- quest-npc/go.mod
+++ quest-npc/go.mod
@@ -8,8 +8,11 @@ require (
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.7 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
-	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
+	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
 	golang.org/x/net v0.0.0-20200927032502-5d4f70055728 // indirect
 	golang.org/x/sys v0.0.0-20200926100807-9d91bd62050c // indirect
-	golang.org/x/text v0.3.3 // indirect
+	golang.org/x/text v0.3.4 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gorm.io/driver/postgres v1.0.5
+	gorm.io/gorm v1.20.5
 )
--- quest-npc/main.go
+++ quest-npc/main.go
@@ -2,10 +2,39 @@ package main
 
 import (
 	"github.com/labstack/echo"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
 	"net/http"
 )
 
+type Story struct {
+	gorm.Model
+	Title string
+	Body  string
+}
+
+var (
+	stories = []Story{
+		{Title: "Hello World", Body: "This is a story about a developer who didn't know what to put here."},
+		{Title: "Another world", Body: "Yeah.. right."},
+	}
+)
+
 func main() {
+
+	dsn := "host=quest-npc-db-svc port=5432 user=gorm password=gorm dbname=gorm sslmode=disable TimeZone=Europe/Sofia"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// Create DB schema
+	db.AutoMigrate(&Story{})
+
+	// Add the dummy records from the var section
+	db.Create(stories)
+
+	// TODO get quest stories from the database instead of those hardcoded values
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Nothing to see here.. move along!\n")
